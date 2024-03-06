2501280c116cb3f3c7336f23ba5ccd027dd36140
Aya <ayagafar88@gmail.com>
2024-03-06 13:00:48 +0200

    let's new start

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { CartComponent } from './components/cart/cart.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ProductsComponent } from './components/products/products.component';
+import { BrandsComponent } from './components/brands/brands.component';
+import { NavbarAuthComponent } from './components/navbar-auth/navbar-auth.component';
+import { NavbarBlankComponent } from './components/navbar-blank/navbar-blank.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CategriesComponent } from './components/categries/categries.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CartComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProductsComponent,
+    BrandsComponent,
+    NavbarAuthComponent,
+    NavbarBlankComponent,
+    FooterComponent,
+    CategriesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
