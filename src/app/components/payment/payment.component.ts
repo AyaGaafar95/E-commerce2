5c4da4ef11b73598550d1938c2ccea36fa40cac4
Aya <ayagafar88@gmail.com>
2024-07-01 06:47:02 +0300

    handle paymentform

--- src/app/components/payment/payment.component.ts
+++ src/app/components/payment/payment.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
-  styleUrls: ['./payment.component.scss']
+  styleUrls: ['./payment.component.scss'],
 })
 export class PaymentComponent {
-
+  checkOutForm: FormGroup = new FormGroup({
+    details: new FormControl(''),
+    phone: new FormControl(''),
+    city: new FormControl(''),
+  });
+  handelForm(): void {
+    console.log(this.checkOutForm.value);
+  }
 }
